21f38d0dd578f590c936ef26fec0fdb86c3805de
COMPUDABO <COMPUDABO@JULES>
2013-10-08 22:13:55 -0500

    adelanto clase Problema

--- Namiki/src/java/controlador/Problema.java
+++ Namiki/src/java/controlador/Problema.java
@@ -10,4 +10,72 @@ package controlador;
  */
 public class Problema {
     
+    Integer idProblema;
+    Integer idCategoria;
+    Integer idUsuario;
+    String descripcion;
+    String fecha;
+    String titulo;
+    String topico;
+    
+    public Problema(Integer idProblema, Integer idCategoria, Integer idUsuario,
+                String fecha, String titulo, String topico) {
+        
+        }
+    
+    public Integer getidProblema() {
+        return this.idProblema;
+    }
+    public Integer getidCategoria() {
+        return this.idCategoria;
+    }
+    public Integer getidUsuario() {
+        return this.idUsuario;
+    }
+    public String getDescripcion() {
+        return this.descripcion;
+    }
+    public String getFecha() {
+        return this.fecha;
+    }
+    public String getTitulo() {
+        return this.titulo;
+    }
+    public String getTopico() {
+        return this.topico;
+    }
+    
+    public void setidProblema() {
+        idProblema = idProblema;
+    }
+    public void setidCategoria() {
+        idCategoria = idCategoria;
+    }
+    public void setidUsuario() {
+        idUsuario = idUsuario;
+    }
+    public void setDescripcion() {
+        descripcion = descripcion;
+    }
+    public void setFecha() {
+        fecha = fecha;
+    }
+    public void setTitulo() {
+        titulo = titulo;
+    }
+    public void setTopico() {
+        topico = topico;
+    }
+    
+    public void registrarProblema() {
+        
+    }
+    
+    public void editarProblema() {
+        
+    }
+    
+    public void borrarProblema() {
+        
+    }
 }
