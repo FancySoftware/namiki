d65196a0722ea6045bbe6f4eaf3534f0cc69adb5
Edd <Edd@MrBrightside>
2013-10-18 14:48:34 -0500

    Nueva clase Usuario

--- Namiki/src/java/controlador/Usuario.java
+++ Namiki/src/java/controlador/Usuario.java
@@ -4,19 +4,24 @@
  */
 package controlador;
 
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.sql.Date;
-import java.util.*;
-import modelo.UsuarioBD;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import modelo.AporteBD;
 import modelo.ProblemaBD;
-import modelo.BaseDatos;
+import modelo.UsuarioBD;
+
 /**
  *
- * @author Jules 
+ * @author Edd
  */
-public class Usuario {
-
-  private int idUsuario;
+public class Usuario extends HttpServlet {
+    
+    private int idUsuario;
   private String nombre;
   private int categoria;
   private String telefono;
@@ -100,4 +105,74 @@ public class Usuario {
          usuario1.guardar(idUsuario, nombre, categoria, telefono, usuario, correo, fechaNacimiento, password);
     }
 
+    /**
+     * Processes requests for both HTTP
+     * <code>GET</code> and
+     * <code>POST</code> methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        try {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet Usuario_</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet Usuario_ at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        } finally {            
+            out.close();
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP
+     * <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP
+     * <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
 }
