83ca48a8e89bc0ed5de76fcdbf7cbd7c366da236
Edd <Edd@MrBrightside>
2013-10-20 20:35:43 -0500

    registro usuario

--- Namiki/src/java/controlador/Usuario.java
+++ Namiki/src/java/controlador/Usuario.java
@@ -20,64 +20,7 @@ import modelo.UsuarioBD;
  * @author Jules
  */
 public class Usuario extends HttpServlet {
-    
-  private int idUsuario;
-  private String nombre;
-  private int categoria;
-  private String telefono;
-  private String correo;
-  private Date fechaNacimiento;
-  private String usuario;
-  private String password;
-  
-//  public Usuario(int idUsuario, String nombre, int categoria, String telefono, 
-//          String usuario, String correo, Date fechaNacimiento, String password) {
-//      
-//  }
-  
-     public int getidUsuario() {
-        return idUsuario;
-     }
-     public String getNombre() {
-        return nombre;
-     }
-     public int getCategoria() {
-        return categoria;
-     }
-     public String getTelefono() {
-        return telefono;
-     }
-     public Date getFecha() {
-        return fechaNacimiento;
-     }
-     public String getUsuario() {
-        return usuario;
-     }
-     public String getPassword() {
-         return password;
-     }
-     
-     public void setidUsuario(int idUsuario) {
-        this.idUsuario = idUsuario;
-     }
-     public void setnombre(String nombre) {
-        this.nombre = nombre;
-     }
-     public void setCategoria(int categoria) {
-        this.categoria = categoria;
-     }
-     public void setDTelefono(String telefono) {
-        this.telefono = telefono;
-     }
-     public void setFecha(Date fechaNacimiento) {
-        this.fechaNacimiento = fechaNacimiento;
-     }
-     public void setUsuario(String usuario) {
-        this.usuario = usuario;
-     }
-     public void setPassword(String password) {
-         this.password = password;
-     }
+
 
     public void getProblemas(int idUsuario) {
          ProblemaBD problema = new ProblemaBD();
@@ -98,11 +41,11 @@ public class Usuario extends HttpServlet {
         usuario1.salir();
     }
 
-    public void registrarUsuario(String nombre, int categoria, String telefono, String usuario,
-          String correo, Date fechaNacimiento, String password) {
+    public void registrarUsuario(String usuario, String password, int categoria, String nombre, String telefono, 
+          String correo, Date fechaNacimiento) {
         
-         UsuarioBD usuario1 = new UsuarioBD(idUsuario, nombre, categoria, telefono, usuario, correo, fechaNacimiento, password);
-         usuario1.guardar(idUsuario, nombre, categoria, telefono, usuario, correo, fechaNacimiento, password);
+         UsuarioBD usuario1 = new UsuarioBD();
+         usuario1.guardar(usuario, password, categoria, nombre, telefono, correo, fechaNacimiento);
     }
 
     /**
@@ -120,6 +63,7 @@ public class Usuario extends HttpServlet {
         response.setContentType("text/html;charset=UTF-8");
         PrintWriter out = response.getWriter();
         try {
+            String answer = "";
             int caso = Integer.parseInt(request.getParameter("form_sumbitted"));
             /*
              * 1 - Registro
@@ -135,22 +79,27 @@ public class Usuario extends HttpServlet {
                     int categoria = Integer.parseInt(request.getParameter("categoria"));
                     if(usuario != null && nombre != null && password != null
                             && correo != null && telefono != null && categoria > 0) {
-                        
+                        java.sql.Date fecha = new java.sql.Date(new java.util.Date().getTime());
+                        registrarUsuario(usuario, password, categoria, nombre, telefono, correo, fecha);
+                        answer = "Success";
+                    } else {
+                        answer = "Failure";
                     }
                     break;
                 case 2:
+//                    String usuario = request.getParameter("usuario");
+//                    String password = request.getParameter("password");
+//                    String answer;
+//                    if(usuario == null || password == null) {
+//                        answer = "Usuario o contraseña invalidos";
+//                    } else {
+//                        answer = "usuario= " + usuario + ";password= "+password;
+//                    }
                     break;
                 default:
                     break;
             }
-            String usuario = request.getParameter("usuario");
-            String password = request.getParameter("password");
-            String answer;
-            if(usuario == null || password == null) {
-                answer = "Usuario o contraseña invalidos";
-            } else {
-                answer = "usuario= " + usuario + ";password= "+password;
-            }
+
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
