96c6efd64506437593374ead2b473a9062897a52
Administrador <Administrador@Win7>
2013-11-24 23:53:38 -0600

    NotificacionBD

--- Namiki/src/java/modelo/NotificacionBD.java
+++ Namiki/src/java/modelo/NotificacionBD.java
@@ -51,6 +51,35 @@ public class NotificacionBD {
       }
   }
 
+  
+ public String[][] tablaNotificaciones(int idUsuario){
+   base.conectar();
+        int numRows = 0;
+        ResultSet cont = base.queryRS("SELECT COUNT(*) numRows FROM notificacion");
+        try {
+            if(cont.next()){
+                numRows = cont.getInt("numRows");
+            } else {
+                System.out.println("Error al contar los registros");
+                numRows = 0;
+            }
+        } catch (SQLException e) {}
+        System.out.println(numRows);
+        ResultSet rs = base.queryRS("SELECT * FROM notificacion"
+                + "WHERE idusuario = "+ idUsuario);
+        String[][] res = new String[numRows][3];
+        int actual = 0;
+        try {
+            while(rs.next()){
+                res[actual][0] = rs.getString("idnotificacion");
+                res[actual][1] = rs.getString("idusuario");
+                res[actual][2] = rs.getString("mensaje");
+                actual++;
+            }
+        } catch (SQLException e) {}
+        return res;
+}
+
   public String getNotificaciones(int idUsurio) {
        try{
            base.conectar();
